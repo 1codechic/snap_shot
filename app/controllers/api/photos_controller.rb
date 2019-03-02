bea98e628a35686f46440f177baaa3f2a064b67e
1codechic <1codechic@devchics.com>
2019-03-01 19:44:53 -0500

    created show method and view

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -4,4 +4,10 @@ class Api::PhotosController < ApplicationController
     @photos = Photo.all
     render 'index.json.jbuilder'
   end
+
+  def show
+    photo_id = params[:id]
+    @photo = Photo.find_by(id: photo_id)
+    render 'show.json.jbuilder'
+  end
 end
