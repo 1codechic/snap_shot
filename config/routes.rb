75961516784b884987c0da229c36b55d590d4f4a
1codechic <1codechic@devchics.com>
2019-03-01 19:28:55 -0500

    created routes for get, post, patch, and delete. Created index method and view for displaying all photos

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/photos" => "photos#index"
+    get "/photos/:id" => "photos#show"
+    post "/photos/new" => "photos#create"
+    patch "/photos/:id" => "photos#update"
+    delete "/photos/:id" => "photos#destroy"
+  end
 end
